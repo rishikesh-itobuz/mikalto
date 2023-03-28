d04d11727311bfb6e15650542ca4db182285e3a3
rishikesh-itobuz <rishikesh@itobuz.com>
2023-03-28 12:36:14 +0530

    Frontend part completed

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,17 +3,36 @@ const port = 8000;
 const path = require("path");
 const fs = require("fs");
 
+
 const filePath = path.join(__dirname + "/dataBase.json");
 const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
 http.createServer((req, res) => {
-    try {
-      res.setHeader("Access-Control-Allow-Origin", "*");
+  try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    const formData = [];
+    req.on("data", (formDataPeices) => {
+      formData.push(formDataPeices);
+    });
+    req.on("end", () => {
+      let totalFormData = Buffer.concat(formData).toString();
+      if (totalFormData.length > 0) {
+        (async function () {
+          await fs.appendFile("./formData.txt", totalFormData);
+        })();
+      }
+    });
+    if(req.url=='/'){
       res.write(JSON.stringify(data));
       res.end();
-    } catch (err) {
+    }
+    
+  } catch (err) {
       console.log(err);
     }
   }).listen(port, () => {
     console.log(`Listening to port ${port} `);
   });
+
+  
--- frontEnd/app.js
+++ frontEnd/app.js
@@ -23,3 +23,35 @@ closeIcon.addEventListener("click", () => {
 });
 
 //Navbar section ends here
+
+//Slider section starts here
+
+let imageCount = 0;
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const sliders = document.getElementsByClassName("sliders");
+
+previousButton.addEventListener('click',function(){
+  if (imageCount === 0)
+    imageCount = sliders.length;
+
+  sliders[imageCount%sliders.length].classList.add('hide');
+  sliders[imageCount-1].classList.remove('hide');
+  imageCount-=1;
+})
+
+nextButton.addEventListener('click',function() {
+  sliders[imageCount].classList.add('hide');
+  if (imageCount === sliders.length-1) {
+        imageCount = -1;
+  } 
+  sliders[imageCount+1].classList.remove('hide');
+  imageCount  += 1;
+  
+})
+
+
+
+
+//Dynamic data of hero section
